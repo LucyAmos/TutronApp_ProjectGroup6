fc30da45f242ef8d4a93103ba636c23e3d7a5080
Dbella01 <55166482+Dbella01@users.noreply.github.com>
2023-06-09 23:52:14 -0400

    added the whole app
    containing the welcome and login xml and java files

---
+++ Tutronapp/app/src/main/java/com/example/tutronapp/WelcomePageActivity.java
@@ -0,0 +1,15 @@
+package com.example.tutronapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+
+    }
+}
