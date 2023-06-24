3d80c1b05d63c6398a6870ea93b4cbc5c3b693a2
Raphaelle Jean-Baptiste <rjean060@uottawa.ca>
2023-06-23 20:14:00 -0400

    Activity admin complain dialogue added ... javafile started for the complain admin ( all commented out) ...
    Issue:
    Button on Ui not aligning...

---
+++ TutronApp/app/src/main/java/com/example/tutrong6/GUI/SuspendDeleteComplaintActivity.java
@@ -0,0 +1,37 @@
+package com.example.tutrong6.GUI;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+/*
+public class SuspendDeleteComplaintActivity( String TutorName, String DateSuspension) {
+
+
+            I started the codebut its not finish
+
+            The tutor and date suspension need to be fixed in the UI
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+            final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+            dialogBuilder.setView(dialogView);
+
+            final TextView  TextTutorName = dialogView.findViewById(R.id.tutorname);
+
+            final TextView TextDateSuspension = dialogView.findViewById(R.id.date_suspension);
+            final Button buttonTemporarySuspension = dialogView.findViewById(R.id.buttonTemporarySuspension);
+            final Button buttonDelete = dialogView.findViewById(R.id.buttonDismiss);
+
+            dialogBuilder.setTitle(productName);
+            final AlertDialog b = dialogBuilder.create();
+            b.show();
+
+
+
+
+}
+
+        */
