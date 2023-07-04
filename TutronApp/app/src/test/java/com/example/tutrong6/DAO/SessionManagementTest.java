920910cc5f609f493fe0a53e18e64edcd5795a71
Raphaelle Jean-Baptiste <rjean060@uottawa.ca>
2023-07-04 00:02:20 -0400

    Complaint.java fails 1 test... please update method CreditCardTest.java fails 1 test ... please update method
     Several Test Added modified
     SessionManagement commented out
    
     DBhelper needs to be finished and cleaned up

---
+++ TutronApp/app/src/test/java/com/example/tutrong6/DAO/SessionManagementTest.java
@@ -0,0 +1,73 @@
+package com.example.tutrong6.DAO;
+
+import android.content.Context;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.example.tutrong6.BEANS.Administrator;
+import com.example.tutrong6.BEANS.Tutor;
+import com.example.tutrong6.BEANS.Student;
+import com.example.tutrong6.BEANS.User;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SessionManagementTest {
+/*
+    private SharedPreferences sharedPreferences;
+    //private SessionManagement sessionMgmnt= new SessionManagement();
+    User user = new User();
+    @Test
+    public void CreateSession(){
+
+
+    }
+
+    /**
+     * Test Case : SaveSession
+     * Description: This test verifies the functionality of the SaveSession method, ensuring that the session is successfully saved.
+     * Expected: Retrieve saved session verify id same as one inputed
+     */
+/*
+    @Test
+    public void SaveSession(){
+        //Step 1 : Set up Session
+        user.setID(123);
+
+        // Step 2 :  Save the session
+        sessionMgmnt.saveSession(user);
+
+        // Step 3: assert session after saved
+        int sessionId = sharedPreferences.getInt("session", -1);
+
+
+        assertEquals(123, sessionId);
+    }
+
+
+
+    /**
+     * Test Case : Remove Session
+     * Description: This test validates the functionality of the RemoveSession method, ensuring that the specified session is removed successfully.
+     * Expected: Created Session removed from database
+     */
+/*
+    @Test
+    public void RemoveSession(){
+        //Step 1 : Set up Session
+        user.setID(755);
+        sessionMgmnt.saveSession(user);
+
+        // Step 2 : Remove the session
+        sessionMgmnt.removeSession();
+
+        // Step 3: assert session after removed
+        int sessionId = sharedPreferences.getInt("session_user", -1);
+
+
+        assertEquals(-1, sessionId);
+    }
+*/
+}
